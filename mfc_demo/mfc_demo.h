bfb2fedcf8b40875d51f623b275aa57b68cea008
WuChao <wuchao01@koolearn.com>
2026-04-28 19:22:57 +0800

    feat: 添加TCP测试工具功能
    实现TCP服务器和客户端功能，包括监听、连接、发送和接收数据
    重构项目结构，添加新的对话框类和资源
    更新项目配置以支持MFC和Winsock库

--- mfc_demo/mfc_demo.h
+++ mfc_demo/mfc_demo.h
@@ -1,3 +1,14 @@
 ﻿#pragma once
 
 #include "resource.h"
+
+class CTcpTestApp : public CWinApp
+{
+public:
+    CTcpTestApp();
+
+public:
+    virtual BOOL InitInstance();
+
+    DECLARE_MESSAGE_MAP()
+};
